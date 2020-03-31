99b2af483b78c973ea6498f776fd0dcca893d710
NinjaFrog <milos281295@gmail.com>
2020-03-31 14:20:13 +0200

    # Initial Commit - .gitignore finished - Starter Content is ignored - Default level changed to Empty Level

---
+++ Source/Building_Escape_2020/Building_Escape_2020GameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Building_Escape_2020GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDING_ESCAPE_2020_API ABuilding_Escape_2020GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
