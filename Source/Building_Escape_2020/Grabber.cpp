cbbdc360de66860410fe0dfcda3e7bc58bad4637
NinjaFrog <milos281295@gmail.com>
2020-04-11 09:52:24 +0200

    # Grabber Component Created & Draw Debug Line - Grabber created and added to out Default Pawn BP - LineTraceEnd calculated and DebugLine has been drawn

--- Source/Building_Escape_2020/Grabber.cpp
+++ Source/Building_Escape_2020/Grabber.cpp
@@ -1,5 +1,12 @@
 #include "Grabber.h"
 
+#include "DrawDebugHelpers.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
+
+
+#define OUT_PARAM // Used to mark OUT parameters in function calls
+
 // Sets default values for this component's properties
 UGrabber::UGrabber()
 {
@@ -26,6 +33,36 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+#pragma region Get Player ViewPoint
+
+	FVector PlayerViewLocation;
+	FRotator PlayerViewRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT_PARAM PlayerViewLocation, 
+		OUT_PARAM PlayerViewRotation
+	);
+
+	//UE_LOG(LogTemp, Warning, TEXT("PlayerViewLocation: %s"), *PlayerViewLocation.ToString())
+	//UE_LOG(LogTemp, Warning, TEXT("PlayerViewRotation: %s"), *PlayerViewRotation.ToString())
+#pragma endregion
+	
+#pragma region Draw Debug Line
+
+	FVector LineTraceStart = PlayerViewLocation;
+	FVector LineTraceEnd = LineTraceStart + PlayerViewRotation.Vector() * GrabberReach;
+
+	DrawDebugLine(
+		GetWorld(), 
+		LineTraceStart,
+		LineTraceEnd,
+		FColor::Green,
+		NULL,
+		NULL,
+		NULL,
+		5
+	);
+#pragma endregion
+
+
 }
 
